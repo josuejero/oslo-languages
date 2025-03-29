22c228d5550b54688d1cd67e944074effd500599
josue <josuejero@hotmail.com>
2025-03-29 15:39:31 -0400

    update

--- src/modules/blog/index.ts
+++ src/modules/blog/index.ts
@@ -1,6 +1,7 @@
 // src/modules/blog/index.ts
 export * from './operations';
-export * from './types';
+// Explicitly export everything *except* BlogError from types
+// to avoid the naming conflict with operations.BlogError
 export * from './hooks';
 
 // This file serves as the main entry point for the blog module
